c55fd7f28dee34034d73aa5f1d64f1fff7f9fe14
Rachel91649 <rachelnjackson@yahoo.com>
2018-05-12 18:32:07 -0400

    adding modules and modifications

--- back-end/app.js
+++ back-end/app.js
@@ -6,6 +6,10 @@ const logger = require('morgan');
 const app = express();
 const port = 8000;
 
+
+
+app.use(logger('dev'));  //only use logger in dev mode and not production
+
 //PORT
 app.listen(port, () => {
   console.log(`port is running on ${port}`);
---
+++ back-end/db/queries.js
@@ -0,0 +1,3 @@
+const db = require("./index"); //requires the db function from index.js in the db folder
+//const authHelpers <-for login authentication
+//const passport <- for login authentication
