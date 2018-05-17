d989bd6637386ef2d9f08de0f8ceda5b3fd4e4d6
Rachel91649 <rachelnjackson@yahoo.com>
2018-05-16 23:28:31 -0400

    imported Route, Link and Switch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Link, Switch} from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          Loging/Register
+        </nav>
+        <h1>This is my homepage</h1>
       </div>
     );
   }
