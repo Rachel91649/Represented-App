456ae2dbb560ac9d25e02939c2ade52e7ccdaec9
Rachel91649 <rachelnjackson@yahoo.com>
2018-05-18 14:04:40 -0400

    added and updated landing page and home component

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+class Landing extends Component {
+  constructor(){
+    super();
+
+  }
+
+
+  render() {
+    return(
+      <div>Landing Page</div>
+    )
+  }
+};
+
+export default Landing;
+
+
