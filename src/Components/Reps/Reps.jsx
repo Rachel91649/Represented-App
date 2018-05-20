5e18f91655b301510d92db95073f676a8c55fa90
Rachel91649 <rachelnjackson@yahoo.com>
2018-05-20 11:15:19 -0400

    created axios call to get all house representatives, printed results to console.

--- src/Components/Reps/Reps.jsx
+++ src/Components/Reps/Reps.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class Reps extends Component { 
   constructor() {
@@ -11,7 +11,16 @@ class Reps extends Component {
 
 
   //axios call to get all reps
-  
+ getAllHouseMemebers = () => {
+  axios.get('https://api.propublica.org/congress/v1/102-115/house/members.json', 
+    {'headers': { 'X-API-Key': 'mSmCDaN4zhN32elyOcV7RbA4z0VmeAXmFpTKICzy'}})
+    .then((response => {
+      console.log(response);
+    }))
+ }
+ componentDidMount() {
+   this.getAllHouseMemebers();
+ }
   render() {
     return(
       <div>
