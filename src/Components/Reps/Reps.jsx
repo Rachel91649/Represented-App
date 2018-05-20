5164b86d725e595f418c81d9d7481716badb36a7
Rachel91649 <rachelnjackson@yahoo.com>
2018-05-20 10:23:19 -0400

    changed reps to stateful component, added axios

--- src/Components/Reps/Reps.jsx
+++ src/Components/Reps/Reps.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const Reps = ({}) => {
-  return(
-    <div>
-      <h1>All Reps Page</h1>
-    </div>
-  )
+class Reps extends Component { 
+  constructor() {
+    super();
+    this.state = {
+      allreps: []
+    }
+  }
+
+
+  //axios call to get all reps
+  
+  render() {
+    return(
+      <div>
+        <h1>All Reps Page</h1>
+      </div>
+    )
+  }
 };
 
 export default Reps;
