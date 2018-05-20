3e54dbb37f42fbe7cb65bb5d2d5bb24eb25d5584
Rachel91649 <rachelnjackson@yahoo.com>
2018-05-19 23:19:08 -0400

    modified Home component to a stateful component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
 
 
-const Home = ({}) => {
-  return(
-    <div>
-      <Nav />
-      <h1>Home Page</h1>
+class Home extends Component { 
+  constructor(){
+    super();
+
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <h1>Home Page</h1>
+        
       
-      {/* <nav>
-        <Link to="/reps">reps</Link>
-      </nav> */}
-      <p>page should have nav bar</p>
-      <p>This page is will house the map</p>
-    </div>
-  )
+        <p>page should have nav bar</p>
+        <p>This page is will house the map</p>
+      </div>
+    )
+  }
 };
 
 export default Home;
